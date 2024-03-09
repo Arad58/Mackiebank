a05cf4032a2433d0b124a0651b717de8c2d3c634
Arad Jalilian <B00163418@mytudublin.ie>
2024-03-09 14:04:09 +0000

    Mackiebank

--- connect.php
+++ connect.php
@@ -1,16 +1,19 @@
 <?php
 
+// Things for the database
 $host = "localhost";
 $dbname = "mackiebank";
 $username = "root";
 $password = "Arad2003!";
 
 try {
+    // Makes new PDO for database
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
-    // Makes PDO error to exception
+    // Makes PDO error for attribute
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // Makes messages if user succeeds or fails
     echo "Connected successfully";
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
